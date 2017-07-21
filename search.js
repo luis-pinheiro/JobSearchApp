404a6e9f917f56fd84b2be52feea5181cfeeb09d
Luis Pinheiro <profitcreations@live.com>
2017-07-21 01:13:57 +0100

    Added react

--- search.js
+++ search.js
@@ -1,11 +1,11 @@
+const Nightmare = require('nightmare');
+const jquery = require('jquery');
+const fs = require('fs');
+const { csvFormat } = require('d3-dsv');
 
 
 
-var Nightmare = require('nightmare');
-var jquery = require('jquery');
-var fs = require('fs');
-
-var jobs = [];
+let jobs = [];
 
 nightmare = Nightmare({
     openDevTools: {
@@ -20,7 +20,7 @@ nightmare = Nightmare({
 });
 
 // Define ItJobs function
-var itJobs = function() {
+let itJobs = function() {
     console.log("======================");
     console.log("= SCRAPING ITJOBS.PT =");
     console.log("======================");
@@ -29,11 +29,11 @@ var itJobs = function() {
             .goto('https://www.itjobs.pt/emprego?location=1&q=frontend&sort=date')
             .wait()
             .evaluate(function() {
-                var itJobs = [];
+                let itJobs = [];
                 $('div.col-xs-12.col-sm-9.col-md-9.altered > div > ul > li').each(function() {
-                    var job = {};
+                    let job = {};
                     job["title"] = $(this).text();
-                    var extractedLink = $(this).find("div.list-title > a").attr("href");
+                    let extractedLink = $(this).find("div.list-title > a").attr("href");
                     job["link"] = "https://www.itjobs.pt" + extractedLink;
                     job["logo"] = $(this).find("div.responsive-container > div > a > img").attr("src");
                     job["company"] = $(this).find("div.list-name > a").text();
@@ -47,7 +47,7 @@ var itJobs = function() {
 };
 
 // define glassdoor
-var glassdoor = function() {
+let glassdoor = function() {
     console.log("==========================");
     console.log("= SCRAPING GLASSDOOR.COM =");
     console.log("==========================");
@@ -56,7 +56,7 @@ var glassdoor = function() {
             .goto('https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=true&clickSource=searchBtn&typedKeyword=front+&sc.keyword=front+end+developer&locT=C&locId=3185896&jobType=')
             .wait()
             .evaluate(function() {
-                var glassJobs = [];
+                let glassJobs = [];
                 $('#MainCol > div > ul > li').each(function() {
                     gb = {};
                     gb["title"] = $(this).find("div > div > div > a").text();
@@ -73,7 +73,7 @@ var glassdoor = function() {
 };
 
 // define indeed.pt
-var indeed = function() {
+let indeed = function() {
     console.log("======================");
     console.log("= SCRAPING INDEED.PT =");
     console.log("======================");
@@ -83,14 +83,14 @@ var indeed = function() {
             .inject('js', './node_modules/jquery/dist/jquery.min.js')
             .wait()
             .evaluate(function() {
-                var indeedJobs = [];
+                let indeedJobs = [];
                 $('.result').each(function() {
                     ind = {};
                     ind["title"] = $(this).find("h2.jobtitle > a").text();
-                    // ind["company"] = $(this).find("div > div.flexbox.empLoc > div").text();
-                    // ind["source"] = "glassdoor.com";
-                    // ind["date"] = $(this).find("div > div.flexbox.empLoc > span.showHH.nowrap > span").text();
-                    // ind["logo"] = $(this).find("div.logoWrap > a > span > img").attr("src");
+                    ind["company"] = $(this).find("span.company > span").text();
+                    ind["source"] = "indeed.pt";
+                    ind["date"] = $(this).find("table > tbody > tr > td > div.result-link-bar-container > div > span.date").text();
+                    ind["description"] = $(this).find(".sumary").text();
 
                     indeedJobs.push(ind);
                 });
@@ -102,21 +102,34 @@ var indeed = function() {
 nightmare
 
     /* itjobs.pt */
-    .use(itJobs())
-    .then(function(itJobs) {
-        jobs.push(itJobs);
-    })
+    // .use(itJobs())
+    // .then(function(itJobs) {
+    //     jobs.push(itJobs);
+    // })
 
     /* glassdoor.com */
-    .then(() => nightmare.use(glassdoor()))
-    .then(function(glassJobs) {
-        jobs.push(glassJobs);
-    })
+    // .then(() => nightmare.use(glassdoor()))
+    // .then(function(glassJobs) {
+    //     jobs.push(glassJobs);
+    // })
 
     /* indeed.pt */
     .then(() => nightmare.use(indeed()))
     .then(function(indeedJobs) {
         jobs.push(indeedJobs);
+        
+    })
+    .then(function(content) {
+         content = JSON.stringify(jobs).replace(/\[|\]|\\n/g, " ");
+
+        // jobs = content;
+
+        fs.writeFile("./jobsearch.json", content, 'utf8', function(err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("The file was saved!");
+        });
         return nightmare.end();
     })
     // .end()
